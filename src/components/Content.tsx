93dc18db1b45c8ee5ae17d02a2ee0bf9e5d6eac9
Paula Santos <paulafap@gmail.com>
2021-10-12 21:35:59 -0300

    correçao de avisos do lint

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -27,7 +27,13 @@ export function Content(props: ContentProps) {
       <main>
         <div className="movies-list">
           {props.movies.map(movie => (
-            <MovieCard key ={movie.imdbID} title={movie.Title} poster={movie.Poster} runtime={movie.Runtime} rating={movie.Ratings[0].Value} />
+            <MovieCard
+              key ={movie.imdbID}
+              title={movie.Title}
+              poster={movie.Poster}
+              runtime={movie.Runtime}
+              rating={movie.Ratings[0].Value}
+            />
           ))}
         </div>
       </main>
