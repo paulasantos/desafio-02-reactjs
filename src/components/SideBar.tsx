88b1c568317f170ec4ec41375388ee3a7eee9d64
Paula Santos <paulafap@gmail.com>
2021-10-12 21:33:25 -0300

    Refaoraçao do App.tsx e criaçao dos componentes SideBar e Content

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { Button } from './Button';
+
+import '../styles/sidebar.scss';
+
+interface SideBarProps {
+  genres: {
+    id: number;
+    name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+    title: string;
+  }[],
+  selectedGenreId: number,
+  handleClickButton: (id: number) => void,
+}
+
+export function SideBar(props: SideBarProps) {
+  return (
+    <nav className="sidebar">
+      <span>Watch<p>Me</p></span>
+
+      <div className="buttons-container">
+        {props.genres.map(genre => (
+          <Button
+            key={String(genre.id)}
+            title={genre.title}
+            iconName={genre.name}
+            onClick={() => props.handleClickButton(genre.id)}
+            selected={props.selectedGenreId === genre.id}
+          />
+        ))}
+      </div>
+
+    </nav>
+  )
 }
